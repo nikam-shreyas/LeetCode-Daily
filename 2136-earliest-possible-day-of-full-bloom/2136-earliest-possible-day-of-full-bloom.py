317b3d8c1824c1e21e2dcf8ffa65cd03a2e9c7ae
Shreyas Nikam <nik4m.5hreyas@gmail.com>
2022-10-29 18:03:28 -0400

    Time: 1798 ms (95.15%), Space: 31 MB (90.62%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
@@ -1,10 +1,6 @@
 import functools
 class Solution:
     def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
-        def compare(x, y):
-            if x[1]==y[1]:
-                return x[0]<y[0]
-            return x[1]<y[1]
         plants = list(zip(growTime, plantTime))
         plants.sort(reverse=True)
         ltime, rtime = 0, 0
