db98713a0213e4e6173ad57b0ec177e3a7289585
Shreyas Nikam <nik4m.5hreyas@gmail.com>
2022-10-31 10:23:35 -0400

    Time: 186 ms (50.50%), Space: 14 MB (6.96%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -2,22 +2,28 @@ class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         r = len(matrix)
         c = len(matrix[0])
-        for col in range(c):
-            i = 0
-            j = col
-            val = matrix[i][j]
-            while i<r and j<c:
-                if matrix[i][j]!=val:
+        prev_row = matrix[0]
+        for row in range(1, r):
+            for i in range(1, c):
+                if matrix[row][i]!=prev_row[i-1]:
                     return False
-                i+=1
-                j+=1
-        for row in range(r):
-            i = row
-            j = 0
-            val = matrix[i][j]
-            while i<r and j<c:
-                if matrix[i][j]!=val:
-                    return False
-                i+=1
-                j+=1
+            prev_row = matrix[row]
         return True
+        #     i = 0
+        #     j = col
+        #     val = matrix[i][j]
+        #     while i<r and j<c:
+        #         if matrix[i][j]!=val:
+        #             return False
+        #         i+=1
+        #         j+=1
+        # for row in range(r):
+        #     i = row
+        #     j = 0
+        #     val = matrix[i][j]
+        #     while i<r and j<c:
+        #         if matrix[i][j]!=val:
+        #             return False
+        #         i+=1
+        #         j+=1
+        # return True
