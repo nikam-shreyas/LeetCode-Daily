24ece08280c8e99aa36c0d83b107896c990e1ec2
Shreyas Nikam <nik4m.5hreyas@gmail.com>
2023-01-02 15:34:51 -0500

    Time: 34 ms (84.34%), Space: 13.9 MB (56.08%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,20 +1,21 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        def cond1(word):
-            return all([ord('A')<=ord(x)<=ord('Z') for x in word])
-            #     return True
-            # return False
-        def cond2(word):
-            return all([ord('a')<=ord(x)<=ord('z') for x in word])
-            #     return True
-            # return False
-        def cond3(word):
-            return ord('A')<=ord(word[0])<=ord('Z') and cond2(word[1:])
-            #     return True
-            # return False
-        word = [x for x in word]
-        return cond1(word) or cond2(word) or cond3(word)
-        # def cond(word):
+        # def cond1(word):
+        #     return all([ord('A')<=ord(x)<=ord('Z') for x in word])
+        #     #     return True
+        #     # return False
+        # def cond2(word):
+        #     return all([ord('a')<=ord(x)<=ord('z') for x in word])
+        #     #     return True
+        #     # return False
+        # def cond3(word):
+        #     return ord('A')<=ord(word[0])<=ord('Z') and cond2(word[1:])
+        #     #     return True
+        #     # return False
+        # word = [x for x in word]
+        # return cond1(word) or cond2(word) or cond3(word)
+        # # def cond(word):
         #     return word[0]==word[0].upper() and word[1:]==word[1:].lower()
         # return word==word.lower() or word==word.upper() or cond(word)
+        return word.islower() or word.isupper() or word.istitle()
             
