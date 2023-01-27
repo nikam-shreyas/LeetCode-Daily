fa399269fc145b20ccbab1e0ba9da80eb293a0af
Shreyas Nikam <nik4m.5hreyas@gmail.com>
2023-01-27 09:47:48 -0500

    Time: 32 ms (76.97%), Space: 13.8 MB (95.90%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,9 @@
 class Solution:
-    dp={}
+    dp={0:1}
     
     def climbStairs(self, n: int) -> int:
         if n in self.dp:
             return self.dp[n]
-        if n==0:
-            return 1
         elif n<0:
             return 0
         self.dp[n] = self.climbStairs(n-1)+self.climbStairs(n-2)
