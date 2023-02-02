dc075345decba9cbcc5e53e27628d9c14f5c892c
Shreyas Nikam <nik4m.5hreyas@gmail.com>
2023-02-02 11:24:36 -0500

    Time: 40 ms (63.42%), Space: 13.8 MB (99.16%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,6 +1,3 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        orderedDict = {letter:counter for counter, letter in enumerate(order)}
-        def compare(word):
-            return [orderedDict[letter] for letter in word]
-        return words == sorted(words, key=compare)
+        return words == sorted(words, key=lambda x: [order.index(letter) for letter in x])
