b06c59c89239912e193877260b2ed95660a8c25a
Shreyas Nikam <nik4m.5hreyas@gmail.com>
2023-02-03 09:23:33 -0500

    Time: 71 ms (22.08%), Space: 20.2 MB (5.05%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = {}
+        def recurse(i, j):
+            if (i, j) in dp:
+                return dp[(i, j)]
+            if j>len(s) or i>j:
+                return False
+            if j==len(s):
+                if s[i:j] in wordDict:
+                    return True
+            if s[i:j] in wordDict:
+                dp[(i, j)] = recurse(j, j+1) or recurse(i, j+1)
+                return dp[(i, j)]
+            else:
+                dp[(i, j)] = recurse(i, j+1)
+                return dp[(i, j)]
+        return recurse(0, 0)
