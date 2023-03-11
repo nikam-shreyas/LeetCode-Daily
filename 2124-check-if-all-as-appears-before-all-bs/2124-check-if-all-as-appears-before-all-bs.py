1f0291f3fd9aa5a1a46a7469110c87cfa505eb32
Shreyas Nikam <nik4m.5hreyas@gmail.com>
2023-03-11 14:59:00 -0500

    Time: 31 ms (72.21%), Space: 13.8 MB (42.34%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -1,4 +1,10 @@
 class Solution:
     def checkString(self, s: str) -> bool:
-        s = list(s)
-        return s==sorted(s)
+        flag = True
+        for char in s:
+            if char=='a':
+                if not flag:
+                    return False
+            else:
+                flag=False
+        return True
