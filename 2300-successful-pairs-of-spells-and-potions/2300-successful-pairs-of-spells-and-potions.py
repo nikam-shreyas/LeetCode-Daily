58111396639746eeec6d5de0ad07e8ace75cc2b1
Shreyas Nikam <nik4m.5hreyas@gmail.com>
2023-04-02 10:58:44 -0400

    Time: 2051 ms (24.64%), Space: 37.1 MB (69.69%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -1,31 +1,20 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        # # sort the potions
-        # # iterate over the spells and find the minimum whose product is >= success
-        # # return m-index of the minimum
-        # # append this to pairs
-        # potions.sort()
-        # def binarysearch(target):
-        #     l=0
-        #     h=len(potions)-1
-        #     while l<h:
-        #         mid = l+(h-l)//2
-        #         if potions[mid]*target>=success:
-        #             h=mid-1
-        #         else:
-        #             l=mid+1
-        #     return l
-        # pairs = []
-        # n = len(spells)
-        # m = len(potions)
-        # for i in range(n):
-        #     index = binarysearch(spells[i])
-        #     print(spells[i], index)
-        #     if index+1>=n or potions[index+1]*spells[i]<success:
-        #         pairs.append(0)
-        #     else:
-        #         pairs.append(m-index)
-        # return pairs
-        
         potions.sort()
-        return [len(potions) - bisect_left(potions, (success + a - 1) // a) for a in spells]
+        ans = []
+        def bSearch(k):
+            low = 0
+            high = len(potions)-1
+            while low<=high:
+                mid = low+(high-low)//2
+                if potions[mid]*k>=success:
+                    high=mid-1
+                else:
+                    low=mid+1
+            return low
+                    
+            
+        n = len(potions)
+        for spell in spells:
+            ans.append(n-bSearch(spell))
+        return ans
