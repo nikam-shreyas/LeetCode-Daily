060f8ff1193c5fd68907dd1d4afab99f76874aa2
Shreyas Nikam <nik4m.5hreyas@gmail.com>
2023-04-13 09:43:13 -0400

    Time: 72 ms (68.11%), Space: 14.1 MB (34.41%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.py
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.py
@@ -1,18 +1,27 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
-        i=0
-        j=0
-        st = []
-        while j<len(popped):
-            if st and st[-1]==popped[j]:
-                st.pop()
-                j+=1
-            elif i<len(pushed) and pushed[i]==popped[j]:
-                i+=1
-                j+=1
-            elif i<len(pushed):
-                st.append(pushed[i])
-                i+=1
-            else:
-                return False
-        return st==[] 
+        # i=0
+        # j=0
+        # st = []
+        # while j<len(popped):
+        #     if st and st[-1]==popped[j]:
+        #         st.pop()
+        #         j+=1
+        #     elif i<len(pushed) and pushed[i]==popped[j]:
+        #         i+=1
+        #         j+=1
+        #     elif i<len(pushed):
+        #         st.append(pushed[i])
+        #         i+=1
+        #     else:
+        #         return False
+        # return st==[] 
+        j = 0
+        stack = []
+        for x in pushed:
+            stack.append(x)
+            while stack and stack[-1] == popped[j]:
+                stack.pop()
+                j += 1
+
+        return j == len(popped)
