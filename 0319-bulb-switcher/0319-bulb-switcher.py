6be5e1ebf1fba30a5906e0c880d8c55256ed2668
Shreyas Nikam <nik4m.5hreyas@gmail.com>
2023-04-27 10:34:34 -0400

    Time: 41 ms (8.42%), Space: 16.2 MB (34.09%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.py
+++ 0319-bulb-switcher/0319-bulb-switcher.py
@@ -1,9 +1,3 @@
 class Solution:
     def bulbSwitch(self, n: int) -> int:
-        bulbs = 0
-        i=1
-        while i*i<=n:
-            i+=1
-            bulbs+=1
-        return bulbs
-        
+        return int(sqrt(n))
