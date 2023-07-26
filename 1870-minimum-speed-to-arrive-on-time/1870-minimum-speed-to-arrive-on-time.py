af88804da1cfec9243d383546db96e9dd5cae9ea
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-07-26 12:57:23 -0400

    Time: 2046 ms (98.01%), Space: 31 MB (25.64%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -1,40 +1,51 @@
+from math import ceil
+
 class Solution:
     def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
-        
+        # Get the number of trains
         n = len(dist)
-        
+
+        # Initialize the minimum possible speed to 1
         minSpeed = 1
-        
+
+        # Calculate the total distance of the journey
         totalDistance = sum(dist)
-        
-        if hour<=n-1:
+
+        # Check if there is enough time to catch all the trains
+        if hour <= n - 1:
             return -1
-        
-        maxSpeed = ceil(totalDistance / (hour-n+1))
-        
-        possible = False
-        
+
+        # Calculate the maximum possible speed
+        maxSpeed = ceil(totalDistance / (hour - n + 1))
+
+        # Variable to store the final answer
         correctSpeed = -1
-        
-        while minSpeed<=maxSpeed:
-            midSpeed = (minSpeed+maxSpeed)//2
-            
+
+        # Binary search loop to find the minimum required speed
+        while minSpeed <= maxSpeed:
+            # Calculate the middle speed to check
+            midSpeed = (minSpeed + maxSpeed) // 2
+
+            # Variable to store the total time required for the commute at midSpeed
             totalTime = 0
-            
-            for i in range(len(dist)-1):
-                totalTime+=ceil(dist[i]/midSpeed)
-            
+
+            # Calculate the total time required for all trains except the last one
+            for i in range(len(dist) - 1):
+                totalTime += ceil(dist[i] / midSpeed)
+
+            # Calculate the time required for the last train
             totalTime += dist[-1] / midSpeed
-            
+
+            # Compare totalTime with the given hour
             if totalTime <= hour:
-                maxSpeed = midSpeed-1
+                # If totalTime is within the given hour, update maxSpeed and correctSpeed
+                maxSpeed = midSpeed - 1
                 correctSpeed = midSpeed
-                
             else:
-                minSpeed = midSpeed+1
-                
+                # If totalTime exceeds the given hour, update minSpeed
+                minSpeed = midSpeed + 1
+
+        # Return the minimum required speed to reach the office on time
         return correctSpeed
-            
-        
-       
-            
+
+# Test cases can be added here to validate the solution
