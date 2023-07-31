f8f862d31e7064f0bbc2a92d152799caa43bfcd1
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-07-31 16:26:51 -0400

    Time: 1213 ms (23.13%), Space: 222.8 MB (6.32%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        dp = {}
+        @lru_cache
+        def recurse(i, j):
+            if (i, j) in dp:
+                return dp[(i, j)]
+            if i<0 and j<0:
+                return 0
+            if i<0 or j<0:
+                if i<0:
+                    dp[(i, j)] = ord(s2[j])+recurse(i, j-1)
+                    return dp[(i, j)]
+                else:
+                    dp[(i, j)] = ord(s1[i])+recurse(i-1, j)
+                    return dp[(i, j)]
+            if s1[i]==s2[j]:
+                dp[(i, j)] = recurse(i-1, j-1)
+                return dp[(i, j)]
+            dp[(i, j)] = min(
+                ord(s1[i])+recurse(i-1, j),
+                ord(s2[j])+recurse(i, j-1)
+            )
+            return dp[(i, j)]
+        return recurse(len(s1)-1, len(s2)-1)
