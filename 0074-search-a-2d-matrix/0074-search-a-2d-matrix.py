5bc13e325881007c3a61aa94e20da8e00d163384
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-08-07 11:12:40 -0400

    Time: 54 ms (76.18%), Space: 17.1 MB (28.37%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,18 +1,31 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Define a recursive function to search for the target in a given submatrix
         def recurse(lowr, highr, lowc, highc):
-            if lowr<=highr and lowc<=highc:
-                midr = lowr+(highr-lowr)//2
-                midc = lowc+(highc-lowc)//2
-                if not(0<=midr<m and 0<=midc<n):
+            # Base case: check if the current submatrix is valid
+            if lowr <= highr and lowc <= highc:
+                # Calculate mid row and mid column
+                midr = lowr + (highr - lowr) // 2
+                midc = lowc + (highc - lowc) // 2
+                
+                # Check if midr and midc are within matrix boundaries
+                if not (0 <= midr < m and 0 <= midc < n):
                     return False
-                if matrix[midr][midc]==target:
+                
+                # Check if the target is found at the middle element
+                if matrix[midr][midc] == target:
                     return True
-                elif matrix[midr][midc]<target:
-                    return recurse(midr+1, highr, lowc, highc) or recurse(lowr, highr, midc+1, highc)
+                # If target is greater, search in the bottom-right and top-right submatrices
+                elif matrix[midr][midc] < target:
+                    return recurse(midr + 1, highr, lowc, highc) or recurse(lowr, highr, midc + 1, highc)
+                # If target is smaller, search in the top-left and bottom-left submatrices
                 else:
-                    return recurse(lowr, midr-1, lowc, highc) or recurse(lowr, highr, lowc, midc-1)
+                    return recurse(lowr, midr - 1, lowc, highc) or recurse(lowr, highr, lowc, midc - 1)
             return False
+        
+        # Get the number of rows and columns in the matrix
         m = len(matrix)
         n = len(matrix[0])
+        
+        # Start the recursive search from the entire matrix
         return recurse(0, m, 0, n)
