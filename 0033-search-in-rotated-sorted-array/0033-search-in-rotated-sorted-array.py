813f3da16716d04eecba2dca2f2dbbd563a80b6f
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-08-08 08:49:20 -0400

    Time: 46 ms (91.79%), Space: 16.8 MB (38.67%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,19 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         low = 0
-        high = len(nums)-1
-        while low<=high:
-            mid = low+(high-low)//2
-            if nums[mid]==target:
+        high = len(nums) - 1
+        
+        # Binary search loop: continue while the search range is valid
+        while low <= high:
+            mid = low + (high - low) // 2  # Calculate the middle index
+            
+            if nums[mid] == target:  # If the middle element is the target, return its index
                 return mid
-            if nums[low]<=nums[mid]:
-                if nums[low]<=target<nums[mid]:
-                    high = mid-1
+            
+            if nums[low] <= nums[mid]:  # If the left half is in increasing order
+                if nums[low] <= target < nums[mid]:  # If target is in the left half
+                    high = mid - 1  # Update the high index to search the left half
                 else:
-                    low = mid+1
-            else:
-                if nums[mid]<target<=nums[high]:
-                    low = mid+1
+                    low = mid + 1  # Update the low index to search the right half
+            else:  # If the right half is in increasing order (and the left is rotated)
+                if nums[mid] < target <= nums[high]:  # If target is in the right half
+                    low = mid + 1  # Update the low index to search the right half
                 else:
-                    high = mid-1
-        return -1
+                    high = mid - 1  # Update the high index to search the left half
+        
+        return -1  # Target not found, return -1
