47148cb60ca6bd6434f3af9a3a114a20e7bacff8
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-08-18 12:39:11 -0400

    Time: 552 ms (10.00%), Space: 18.5 MB (15.22%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.py
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -1,15 +1,31 @@
+from collections import defaultdict
+
 class Solution:
-    def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
-        adj = defaultdict(set)
-        for start, end in roads:
-            adj[start].add((start, end))
-            adj[end].add((start, end))
-        
-        maxrank = 0
-        # print(adj)
-        for i in range(n-1):
-            for j in range(i+1, n):
-                # print(i, j)
-                rank = len(adj[i].union(adj[j]))
-                maxrank = max(maxrank, rank)
-        return maxrank
+
+    def maximalNetworkRank(self, numCities: int, roads: List[List[int]]) -> int:
+
+        # Create an adjacency list to represent connections between cities
+        adjList = defaultdict(set) 
+
+        for startCity, endCity in roads:
+
+            # Add road connections in both directions
+            adjList[startCity].add((startCity, endCity))  
+            adjList[endCity].add((startCity, endCity))
+
+        maxRank = 0
+
+        # Loop through all pairs of cities to find max connections
+        for city1 in range(numCities-1):
+            
+            for city2 in range(city1+1, numCities):
+
+                # Get all combined road connections between two cities
+                connections = adjList[city1].union(adjList[city2])
+
+                rank = len(connections)
+
+                # Update max rank
+                maxRank = max(maxRank, rank)
+
+        return maxRank
