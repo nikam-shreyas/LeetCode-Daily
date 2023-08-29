264dcee3ab259273e910f34918f1d07515f8783f
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-08-29 10:11:01 -0400

    Time: 240 ms (27.41%), Space: 22.5 MB (19.28%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -1,22 +1,33 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        leftNs = [0]
-        rightYs = [0]
-        n = len(customers)
-        for i in range(n):
-            if customers[i]=='N':
-                leftNs.append(leftNs[-1]+1)
+        # Initialize counters to track unhappy customers
+        left_no = [0] 
+        right_yes = [0]
+
+        num_customers = len(customers)
+
+        # Populate left_no_shoes list counting "N" customers from left
+        for i in range(num_customers):
+            if customers[i] == 'N':
+                left_no.append(left_no[-1] + 1) 
             else:
-                leftNs.append(leftNs[-1])
-            if customers[n-i-1]=='Y':
-                rightYs.append(rightYs[-1]+1)
+                left_no.append(left_no[-1])
+
+        # Populate right_yes_shoes list counting "Y" customers from right  
+        for i in range(num_customers-1, -1, -1):
+            if customers[i] == 'Y':
+                right_yes.append(right_yes[-1] + 1)
             else:
-                rightYs.append(rightYs[-1])
-        minI = -1
-        minVal = inf
-        for i in range(n+1):
-            if rightYs[n-i]+leftNs[i]<minVal:
-                minVal = rightYs[n-i]+leftNs[i]
-                minI = i
-        return minI
-            
+                right_yes.append(right_yes[-1])
+
+        # Find best closing time 
+        best_time = -1
+        min_unhappy = float("inf")
+
+        for i in range(num_customers + 1):
+            unhappy_custs = right_yes[num_customers - i] + left_no[i]
+            if unhappy_custs < min_unhappy:
+                min_unhappy = unhappy_custs
+                best_time = i
+
+        return best_time
