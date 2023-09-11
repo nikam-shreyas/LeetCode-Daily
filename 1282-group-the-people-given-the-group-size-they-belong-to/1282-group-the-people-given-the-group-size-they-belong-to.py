53b4e2f552d0d3819dfc333811c5c2f8ba9d724c
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-09-11 07:47:52 -0400

    Time: 72 ms (74.33%), Space: 16.4 MB (64.84%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,20 +1,29 @@
 from collections import Counter
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
-        gs = Counter(groupSizes)
-        for key, val in gs.items():
-            temp = val//key
-            gs[key] = [[] for _ in range(temp)]
-        its = {i:0 for i in gs.keys()}
-        for person in range(len(groupSizes)):
-            group = groupSizes[person]
-            if len(gs[group][its[group]])==group:
-                its[group]+=1
-            gs[group][its[group]].append(person)
-        ans = []
-        for _, val in gs.items():
-            ans.extend(val)
-        return ans
-        
-            
-        
+        group_counts = Counter(groupSizes)
+    
+        # Initialize the result groups
+        for size, count in group_counts.items():
+            num_groups = count // size
+            group_counts[size] = [[] for _ in range(num_groups)]
+
+        group_indices = {size: 0 for size in group_counts}
+
+        # Add each person to the appropriate group
+        for person, group in enumerate(groupSizes):
+
+          # Check if current group is full
+          if len(group_counts[group][group_indices[group]]) == group:
+            group_indices[group] += 1
+
+          # Add person to group  
+          group_counts[group][group_indices[group]].append(person)
+
+        result = []
+
+        # Flatten groups into output  
+        for groups in group_counts.values():
+            result.extend(groups)
+
+        return result
