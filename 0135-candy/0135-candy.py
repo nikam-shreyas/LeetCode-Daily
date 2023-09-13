f0d2460ac407a24058da7564722abe19af7c7f5a
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-09-13 10:00:26 -0400

    Time: 148 ms (51.12%), Space: 19.4 MB (40.19%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,14 +1,29 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        n = len(ratings)
-        left = [1 for i in range(n)]
-        right = [1 for i in range(n)]
-        for i in range(1, n):
-            if ratings[i]>ratings[i-1]:
-                left[i]=left[i-1]+1
-            if ratings[n-1-i]>ratings[n-i]:
-                right[n-1-i]=right[n-i]+1
-        candies = 0
-        for i in range(n):
-            candies+=max(left[i], right[i])
-        return candies
+        # Number of children
+        num_children = len(ratings)
+
+        # Initialize left and right candy counts to 1 for each child
+        left_candy = [1] * num_children  
+        right_candy = [1] * num_children
+
+        # Iterate through ratings from left to right
+        for i in range(1, num_children):
+          # If current rating is greater than previous, 
+          # increment left candy count
+            if ratings[i] > ratings[i-1]:
+                left_candy[i] = left_candy[i-1] + 1
+
+          # If current rating is greater than previous,
+          # increment right candy count 
+            if ratings[num_children-i-1] > ratings[num_children-i]:
+                right_candy[num_children-i-1] = right_candy[num_children-i] + 1
+
+        # Initialize variable to track total candies needed  
+        total_candies = 0
+
+        # Take maximum of left and right candy counts for each child
+        for i in range(num_children):
+            total_candies += max(left_candy[i], right_candy[i])
+
+        return total_candies
