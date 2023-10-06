1445da1959b4ac57ee4d5470f19f6217168e8ffe
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-10-06 11:00:49 -0400

    Time: 31 ms (95.63%), Space: 16.2 MB (58.98%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,9 +1,12 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        case = [0,0,1,2,4,6,9]
-        if n < 7:
-            return case[n]
-        dp = case + [0] * (n-6)
-        for i in range(7, n+1):
-            dp[i] = 3*dp[i-3]
-        return dp[-1]
+        if n <= 3:
+            return n - 1
+        
+        if n % 3 == 0:
+            return 3 ** (n // 3)
+        
+        if n % 3 == 1:
+            return 3 ** (n // 3 - 1) * 4
+        
+        return 3 ** (n // 3) * 2
