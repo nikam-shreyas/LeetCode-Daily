09c8d324a600335b77bbe9541300cb6fbaac16f4
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-10-13 10:06:01 -0400

    Time: 58 ms (75.65%), Space: 18.7 MB (8.10%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -10,3 +10,10 @@ class Solution:
             dp[step] = min(recurse(step+1, currcost), recurse(step+2, currcost))+cost[step]
             return dp[step]
         return min(recurse(0, 0), recurse(1, 0))
+        
+        n = len(cost)
+        dp = [cost[i] for i in range(n)]
+        for i in range(2, n):
+            dp[i] += min(dp[i-1, dp[i-2]])
+        return dp[-1]
+        
