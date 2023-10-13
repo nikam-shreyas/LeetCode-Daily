2ee367ef7af07f1e465d2b2d60d7ebc8aa3b1cf2
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-10-13 10:08:49 -0400

    Time: 54 ms (90.45%), Space: 16.4 MB (52.53%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,19 +1,19 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        dp = {}
-        def recurse(step, currcost):
-            if step in dp:
-                return dp[step]
-            if step>=len(cost):
-                dp[step] = currcost
-                return dp[step]
-            dp[step] = min(recurse(step+1, currcost), recurse(step+2, currcost))+cost[step]
-            return dp[step]
-        return min(recurse(0, 0), recurse(1, 0))
+        # dp = {}
+        # def recurse(step, currcost):
+        #     if step in dp:
+        #         return dp[step]
+        #     if step>=len(cost):
+        #         dp[step] = currcost
+        #         return dp[step]
+        #     dp[step] = min(recurse(step+1, currcost), recurse(step+2, currcost))+cost[step]
+        #     return dp[step]
+        # return min(recurse(0, 0), recurse(1, 0))
         
         n = len(cost)
-        dp = [cost[i] for i in range(n)]
-        for i in range(2, n):
-            dp[i] += min(dp[i-1, dp[i-2]])
+        dp = [cost[i] for i in range(n)]+[0]
+        for i in range(2, n+1):
+            dp[i] += min(dp[i-1], dp[i-2])
         return dp[-1]
         
