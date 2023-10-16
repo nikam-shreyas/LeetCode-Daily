76acacd6dbd6612a45a12b9cff05c45fe0561998
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-10-16 17:44:38 -0400

    Time: 50 ms (6.13%), Space: 16.2 MB (81.98%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -2,16 +2,25 @@ class Solution:
     def __init__(self):
         self.triangle = [[1]]
     def getRow(self, rowIndex: int) -> List[int]:
-        if rowIndex<len(self.triangle):
-            return self.triangle[rowIndex]
-        i = len(self.triangle)
-        temp = [0]+self.triangle[-1]+[0]
-        while i<=rowIndex:
-            temp2 = [0]
+        # if rowIndex<len(self.triangle):
+        #     return self.triangle[rowIndex]
+        # i = len(self.triangle)
+        # temp = [0]+self.triangle[-1]+[0]
+        # while i<=rowIndex:
+        #     temp2 = [0]
+        #     for j in range(1, len(temp)):
+        #         temp2.append(temp[j]+temp[j-1])
+        #     temp2.append(0)
+        #     temp = temp2
+        #     self.triangle.append(temp[1:-1])
+        #     i+=1
+        # return self.triangle[rowIndex]
+        if rowIndex == 0:
+            return [1]
+        start = [1]
+        for i in range(rowIndex):
+            temp = [0]+start+[0]
+            start = []
             for j in range(1, len(temp)):
-                temp2.append(temp[j]+temp[j-1])
-            temp2.append(0)
-            temp = temp2
-            self.triangle.append(temp[1:-1])
-            i+=1
-        return self.triangle[rowIndex]
+                start.append(temp[j]+temp[j-1])
+        return start
