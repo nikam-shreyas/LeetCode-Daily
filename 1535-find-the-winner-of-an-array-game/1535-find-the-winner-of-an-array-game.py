0cc9225f4140dbff9e4a36920514cd80d470d517
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-11-06 14:58:32 -0500

    Time: 516 ms (77.03%), Space: 29.6 MB (84.26%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
@@ -1,29 +1,19 @@
 from heapq import heapify, heappush, heappop
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
-        h = [(index, num, 0) for index, num in enumerate(arr)]
-        l = len(arr)
-        if k>l:
-            return max(arr)
-        heapify(h)
-        while True:
-            index1, num1, count1 = heappop(h)
-            index2, num2, count2 = heappop(h)
-            if num1>num2:
-                count1+=1
-                if count1==k:
-                    return num1
-                heappush(h, (0, num1, count1))
-                heappush(h, (l+1, num2, count2))
-                l+=1
+        n = len(arr)
+        score = 0
+        cur = arr[0]
+        for i in range(1, n):
+            if arr[i] < cur:
+                score += 1
             else:
-                count2+=1
-                if count2==k:
-                    return num2
-                heappush(h, (0, num2, count2))
-                heappush(h, (l+1, num1, count1))
-                l+=1
+                if score >= k:
+                    return cur
+                cur = arr[i]
+                score = 1
         
+        return cur
                 
         
         
