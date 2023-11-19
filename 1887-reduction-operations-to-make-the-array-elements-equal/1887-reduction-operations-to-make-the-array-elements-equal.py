018c693837c59b5f96b7c5af1f8fde455f190841
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-11-19 16:25:06 -0500

    Time: 815 ms (88.54%), Space: 27.4 MB (34.90%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -2,7 +2,6 @@ from collections import Counter
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
         counts = Counter(nums)
-        h = list(zip(counts.keys(), counts.values()))
         count = 0
         curr_count = 0
         keys = sorted(counts.keys(), reverse=True)[:-1]
