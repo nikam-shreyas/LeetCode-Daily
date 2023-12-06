096fae3cadead0b0fb70116b61d7b5a2fbc7f953
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-12-06 18:35:11 -0500

    Time: 43 ms (36.87%), Space: 16.3 MB (40.52%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,14 +1,20 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        amount = 0
-        start = 1
-        temp = start
-        for i in range(1, n+1):
-            amount+=temp
-            if i%7==0:
-                temp = start
-                start+=1
-            temp+=1
-            # n-=1
+        # amount = 0
+        # start = 1
+        # temp = start
+        # for i in range(1, n+1):
+        #     amount+=temp
+        #     if i%7==0:
+        #         temp = start
+        #         start+=1
+        #     temp+=1
+        #     # n-=1
+        # return amount
+        num_completed_weeks = n//7
+        amount=num_completed_weeks*28
+        amount+=((num_completed_weeks-1)*(num_completed_weeks))//2*7
+        remaining_days = n%7
+        amount+=(remaining_days*(remaining_days+1)//2)+(num_completed_weeks*remaining_days)
         return amount
         
