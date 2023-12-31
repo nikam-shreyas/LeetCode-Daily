7833fffbe54e12d66076acbeee63b539de9a83f6
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2023-12-31 12:10:04 -0500

    Time: 61 ms (16.10%), Space: 17.1 MB (12.44%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.py
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.py
@@ -1,15 +1,11 @@
 class Solution:
     def maxLengthBetweenEqualCharacters(self, s: str) -> int:
-        dp = {}
-        def recurse(i, j):
-            if (i, j) in dp:
-                return dp[(i, j)]
-            if i>j or i>len(s) or j<0:
-                dp[(i, j)] = -1
-                return -1
-            if s[i]==s[j]:
-                dp[(i, j)] = j-i-1
-                return j-i-1
-            dp[(i, j)] = max(recurse(i+1, j), recurse(i, j-1))
-            return dp[(i, j)]
-        return recurse(0, len(s)-1)
+        ht = {}
+        maxlen = -1
+        for i, char in enumerate(s):
+            if char in ht:
+                maxlen = max(maxlen, i-ht[char]-1)
+            else:
+                ht[char] = i
+        return maxlen
+                
