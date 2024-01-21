70a6cc7202bcc3fc9628315b059e9d550c4516cc
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2024-01-21 11:47:37 -0500

    Time: 35 ms (78.62%), Space: 16.5 MB (56.96%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,16 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [[-1 for _ in range(2)] for _ in range(n)]
-        def recurse(i, j):
-            if i>=n:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if j:
-                dp[i][j]=recurse(i+1, abs(j-1))
-            else:
-                dp[i][j]=max(recurse(i+1, j), recurse(i+1, abs(j-1))+nums[i])
-            return dp[i][j]
-        return recurse(0, 0)
+        dp = [[-1 for _ in range(2)] for _ in range(n+1)]
+        for i in range(1, n+1):
+            dp[i][0] = max(nums[i-1], nums[i-1]+dp[i-1][1])
+            dp[i][1] = max(dp[i-1][0], dp[i-1][1])
+        return max(dp[-1][0], dp[-1][1])
+#         def recurse(i, j):
+#             if i>=n:
+#                 return 0
+#             if dp[i][j]!=-1:
+#                 return dp[i][j]
+#             if j:
+#                 dp[i][j]=recurse(i+1, abs(j-1))
+#             else:
+#                 dp[i][j]=max(recurse(i+1, j), recurse(i+1, abs(j-1))+nums[i])
+#             return dp[i][j]
+#         return recurse(0, 0)
+
+        
         
