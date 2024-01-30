656b69d7a6590a199802668934cedb70c3975013
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2024-01-30 09:39:19 -0500

    Time: 58 ms (95.28%), Space: 17.3 MB (58.04%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,36 +1,19 @@
+import operator
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        i = 0
+        ops = {
+            '+':operator.add,
+            '-':operator.sub,
+            '*':operator.mul,
+            '/':lambda x1, x2:int(x1/x2)
+              }
         st = []
-        def isOp(op):
-            if op in ['+' ,'-' ,'/', '*']:
-                return True
-            return False
-        
-        def doOp(num2, num1, op):
-            if op=='+':
-                return num1+num2
-            elif op=='-':
-                return num1-num2
-            elif op=='*':
-                return num1*num2
-            elif op=='/':
-                return int(num1/num2)
-            
-        while True:
-            if i==len(tokens):
-                break
-                
-            # print(tokens[i], st)
-            if isOp(tokens[i]):
-                num1 = st.pop()
-                num2 = st.pop()
-                op = tokens[i]
-                st.append(doOp(num1, num2, op))
+        for token in tokens:
+            if token not in ops:
+                st.append(int(token))
             else:
-                st.append(int(tokens[i]))
-            i+=1
-            
-        return st[0]
-            
-            
+                
+                num2 = st.pop()
+                num1 = st.pop()
+                st.append(ops[token](num1, num2))
+        return st.pop()
