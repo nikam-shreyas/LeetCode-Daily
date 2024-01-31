69ea0f74561c0f39797071bad0cd8a080e3c5ae2
Shreyas Nikam <shreyas.s.nikam@gmail.com>
2024-01-31 09:57:07 -0500

    Time: 915 ms (62.34%), Space: 31.1 MB (74.70%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,20 +1,35 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # st = []
+        # n = len(temperatures)
+        # ans = []
+        # for i in range(n-1, -1, -1):
+        #     curr = temperatures[i]
+        #     # print(curr, st)
+        #     count = 0
+        #     while st and st[-1][0]<=curr:
+        #         st.pop()
+        #     # print(st)
+        #     if st:
+        #         ans.append(st[-1][1]-i)
+        #     else:
+        #         ans.append(0)
+        #     # print(ans)
+        #     # print()
+        #     st.append((curr, i))
+        # return ans[::-1]
         st = []
-        n = len(temperatures)
         ans = []
-        for i in range(n-1, -1, -1):
-            curr = temperatures[i]
-            # print(curr, st)
-            count = 0
-            while st and st[-1][0]<=curr:
+        
+        for i in range(len(temperatures)-1, -1, -1):
+            temp = temperatures[i]
+            while st:
+                if st[-1][0]>temp:
+                    break
                 st.pop()
-            # print(st)
             if st:
                 ans.append(st[-1][1]-i)
             else:
                 ans.append(0)
-            # print(ans)
-            # print()
-            st.append((curr, i))
+            st.append((temp, i))
         return ans[::-1]
